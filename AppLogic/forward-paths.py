83f192491d0bc8f41a2271c661502004e86e18bf
heba mostafa <96655351+hebamost@users.noreply.github.com>
2022-04-24 17:12:35 +0200

    Update forward-paths.py

--- AppLogic/forward-paths.py
+++ AppLogic/forward-paths.py
@@ -1,1 +1,26 @@
+def turn_to_list(gains):
+    return list(itertools.product(*gains))
+    
 
+def multiply_gains(fwd_paths_gain):
+    total_gains= []
+    for i in fwd_paths_gain:
+        x_gain=1
+        for j in i:
+            x_gain*=j
+        total_gains.append(x_gain)
+    return total_gains
+
+
+def get_gains(graph,path):
+    gains = []
+    length = len(path) - 1
+    mod = length +  1 
+    for i in range(length):
+         edge = graph.get_edge_data(path[i], path[(i + 1) % mod])
+         weights = []
+         for j in range(len(edge)):
+             weights.append(edge[j]['weight'])
+         gains.append(weights)
+    gains = turn_to_list(gains)
+    return multiply_gains(gains)
