22248c4131a735149f848ac3dd06fff39c27ba61
heba mostafa <96655351+hebamost@users.noreply.github.com>
2022-04-25 14:27:47 +0200

    Update forward-paths.py

--- GUI/AppLogic/forward-paths.py
+++ GUI/AppLogic/forward-paths.py
@@ -1,26 +1,39 @@
-def turn_to_list(gains):
-    return list(itertools.product(*gains))
-    
+import itertools
+import networkx as nx
+import sympy as sym
 
-def multiply_gains(fwd_paths_gain):
-    total_gains= []
+
+def remove_duplicates(paths):
+    res = []
+    for i in paths:
+        if i not in res:
+            res.append(i)
+    return res
+
+
+def gain_multiply(gains):
+    return list(itertools.product(*gains))
+
+
+def get_all_fwdpath_gains(fwd_paths_gain):
+    total_gains = []
     for i in fwd_paths_gain:
-        x_gain=1
+        x_gain = 1
         for j in i:
-            x_gain*=j
+            x_gain *= j
         total_gains.append(x_gain)
     return total_gains
 
 
-def get_gains(graph,path):
+def get_gains(graph, path):
     gains = []
     length = len(path) - 1
-    mod = length +  1 
+    mod = length + 1
     for i in range(length):
-         edge = graph.get_edge_data(path[i], path[(i + 1) % mod])
-         weights = []
-         for j in range(len(edge)):
-             weights.append(edge[j]['weight'])
-         gains.append(weights)
-    gains = turn_to_list(gains)
-    return multiply_gains(gains)
+        edge = graph.get_edge_data(path[i], path[(i + 1) % mod])
+        weights = []
+        for j in range(len(edge)):
+            weights.append(edge[j]['weight'])
+        gains.append(weights)
+    gains = gain_multiply(gains)
+    return get_all_fwdpath_gains(gains)
